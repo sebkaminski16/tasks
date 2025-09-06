94eed99f93aa27d36a89c4cab351139f02e85474
Sebastian Kaminski <sebkaminski16@gmail.com>
2025-09-06 14:10:41 +0200

    Add email scheduler

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleMailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+    private final SimpleMailService simpleMailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        simpleMailService.send(
+                Mail.builder()
+                        .mailTo(adminConfig.getAdminMail())
+                        .subject(SUBJECT)
+                        .message("Currently in database you got: " + size + " tasks")
+                        .build()
+        );
+    }
+}
---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import com.crud.tasks.trello.config.AdminConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TrelloService {
+
+    public static final String SUBJECT = "Tasks: New Trello card";
+    private final TrelloClient trelloClient;
+    private final SimpleMailService emailService;
+    private final AdminConfig adminConfig;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+        Optional.ofNullable(newCard).ifPresent(card -> emailService.send(
+                Mail.builder()
+                        .mailTo(adminConfig.getAdminMail())
+                        .subject(SUBJECT)
+                        .message("New card: " + trelloCardDto.getName() + " has been created on your Trello account")
+                        .build()
+        ));
+        return newCard;
+    }
+}
