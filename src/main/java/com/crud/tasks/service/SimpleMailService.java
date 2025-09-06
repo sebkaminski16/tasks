ca31be74919ef40e524e6f5516b5ff36dc65e0fe
Sebastian Kaminski <sebkaminski16@gmail.com>
2025-09-06 12:50:42 +0200

    Add simple mail service

---
+++ src/main/java/com/crud/tasks/service/SimpleMailService.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SimpleMailService {
+
+    private final JavaMailSender javaMailSender;
+
+    private SimpleMailMessage createMailMessage(Mail mail) {
+        SimpleMailMessage smm = new SimpleMailMessage();
+        smm.setTo(mail.getMailTo());
+        smm.setSubject(mail.getSubject());
+        smm.setText(mail.getMessage());
+        return smm;
+    }
+
+    public void send(Mail mail) {
+        log.info("Starting email preparation...");
+
+        try {
+            SimpleMailMessage smm = createMailMessage(mail);
+            javaMailSender.send(smm);
+            log.info("Email has been sent!");
+        } catch (MailException e) {
+            log.error("Failed to send an email: " + e.getMessage(), e);
+        }
+    }
+
+}
