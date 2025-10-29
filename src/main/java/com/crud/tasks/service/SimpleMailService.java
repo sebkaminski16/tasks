0c6339792624982b7f1b69e1731a7f8653481487
P K <sebkaminski16@gmail.com>
2025-10-29 15:45:50 +0100

    change

--- src/main/java/com/crud/tasks/service/SimpleMailService.java
+++ src/main/java/com/crud/tasks/service/SimpleMailService.java
@@ -3,12 +3,9 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -20,24 +17,22 @@ public class SimpleMailService {
 
     private final JavaMailSender javaMailSender;
 
-    @Autowired
-    private MailCreatorService mailCreatorService;
-
-    private MimeMessagePreparator createMimeMessage(final Mail mail) {
-        return mimeMessage -> {
-            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-            messageHelper.setTo(mail.getMailTo());
-            messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
-        };
+    private SimpleMailMessage createMailMessage(Mail mail) {
+        SimpleMailMessage smm = new SimpleMailMessage();
+        smm.setTo(mail.getMailTo());
+        smm.setSubject(mail.getSubject());
+        smm.setText(mail.getMessage());
+        Optional<String> mailCc = Optional.ofNullable(mail.getCcTo());
+        mailCc.ifPresent(smm::setCc);
+        return smm;
     }
-
 
     public void send(Mail mail) {
         log.info("Starting email preparation...");
 
         try {
-            javaMailSender.send(createMimeMessage(mail));
+            SimpleMailMessage smm = createMailMessage(mail);
+            javaMailSender.send(smm);
             log.info("Email has been sent!");
         } catch (MailException e) {
             log.error("Failed to send an email: " + e.getMessage(), e);
